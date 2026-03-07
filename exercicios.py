88ee7a717633c74aade241544475a622179003f0
rafaelmello082 <rafaelmello082@gmail.com>
2026-03-07 14:50:15 -0300

    adicionado ex10 lista exercicios trilha 01 aula 02

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,15 @@
+import math
+
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-num1 = int(input('Digite o 1o número: '))
+"""num1 = int(input('Digite o 1o número: '))
 num2 = int(input('Digite o 2o número: '))
 div_inteira = num1 // num2
-print(f'A divisão inteira dos dois números é {div_inteira}')
+print(f'A divisão inteira dos dois números é {div_inteira}')"""
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
@@ -18,6 +20,9 @@ print(f'A divisão inteira dos dois números é {div_inteira}')
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+raio = float(input("Digite o raio: "))
+area_circulo = math.pi * raio**2
+print(f'Área do círculo: {area_circulo:.2f}')
 
 # #### Strings (`str`)
 
